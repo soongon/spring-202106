1c745c9ab8c079d3d4b940d5bad7496e407d4415
soongon <soongon@hucloud.co.kr>
2021-06-30 17:13:26 +0900

    initial commit

--- src/main/java/kr/re/kitri/hello/dao/PostRepository.java
+++ src/main/java/kr/re/kitri/hello/dao/PostRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 public interface PostRepository {
     List<Post> selectAllPosts();
     void insertPost(Post post);
+    Post selectPostById(long userId);
 }
---
+++ src/test/java/kr/re/kitri/hello/dao/PostRepositoryTests.java
@@ -0,0 +1,33 @@
+package kr.re.kitri.hello.dao;
+
+import kr.re.kitri.hello.model.Post;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class PostRepositoryTests {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    public void testSelectAllPosts() {
+        List<Post> list = postRepository.selectAllPosts();
+        Assertions.assertEquals(1, list.get(0).getPostId());
+    }
+    @Test
+    public void testInsertPost() {
+        Post post = new Post(10000, "aaa", "bbbb", "ccc", 111);
+        postRepository.insertPost(post);
+    }
+    @Test
+    public void testSelectPostById() {
+        Post post = postRepository.selectPostById(1);
+
+    }
+}
