9a51cc33d967856232fdd5a5f9970ce9ac57e9ee
soongon <soongon@hucloud.co.kr>
2021-06-30 15:51:51 +0900

    initial commit

--- src/main/java/kr/re/kitri/hello/dao/PostRepository.java
+++ src/main/java/kr/re/kitri/hello/dao/PostRepository.java
@@ -1,21 +1,14 @@
 package kr.re.kitri.hello.dao;
 
 import kr.re.kitri.hello.model.Post;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.DriverManager;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class PostRepository {
-
-    public List<Post> selectAllPosts() {
-        return null;
-    }
-
-    public void insertPost(Post post) {
-        // DB 에 insert...
-        System.out.println("인서트 완료.." + post);
-    }
+@Mapper
+public interface PostRepository {
+    List<Post> selectAllPosts();
+    void insertPost(Post post);
 }
